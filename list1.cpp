747b6bf6decf0078280d49cd2086601097ecb22c
Abdalla7798 <a.b.abdalla305@gmail.com>
2021-08-14 10:32:21 +0200

    create project

---
+++ list1.cpp
@@ -0,0 +1,11 @@
+#include "list1.h"
+
+List1::List1()
+{
+    //ctor
+}
+
+List1::~List1()
+{
+    //dtor
+}
---
+++ list1.h
@@ -0,0 +1,16 @@
+#ifndef LIST1_H
+#define LIST1_H
+
+
+class List1
+{
+    public:
+        List1();
+        virtual ~List1();
+
+    protected:
+
+    private:
+};
+
+#endif // LIST1_H
